0c432fb4535f4c885955ccb0b9c0f7af6514f626
311 <ismoilov92@gmail.com>
2022-08-04 23:13:58 +0300

    Article form

---
+++ src/views/CreateArticle.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <app-article-form
+    :initial-values="initialValues"
+    :errors="validationErrors"
+    :is-Submiting="isSubmitting"
+    @articleSumit="onSubmit"
+    />
+  </div>
+</template>
+
+<script>
+import AppArticleForm from '@/components/ArticleForm'
+export default {
+  name: "AppCreateArticle",
+  components:{
+    AppArticleForm
+  },
+  data(){
+    return{
+      initialValues:{
+        title:'',
+        description:'',
+        body:'',
+        tagList:[]
+      },
+      validationErrors:null,
+      isSubmitting:false
+    }
+  },
+  methods:{
+    onSubmit(data){
+      console.log('onSubmit ', data)
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
